7051e8c077e9e669087ef64aac1bdddff2d4c388
cheese-bit <63467622+cheese-bit@users.noreply.github.com>
2020-05-28 12:27:21 -0700

    Add files via upload

---
+++ ActionFilters/GlobalRouting.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace trash.ActionFilter
+{
+    public class GlobalRouting : IActionFilter
+    {
+        private readonly ClaimsPrincipal _claimspPrincipal;
+        public GlobalRouting(ClaimsPrincipal claimsPrincipal)
+        {
+            _claimspPrincipal = claimsPrincipal;
+        }
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var controller = context.RouteData.Values["controller"];
+            if (controller.Equals("Home"))
+            {
+                if (_claimspPrincipal.IsInRole("Customer"))
+                {
+                    context.Result = new RedirectToActionResult("Index", "Customers", null);
+                }
+                else if (_claimspPrincipal.IsInRole("Employee"))
+                {
+                    context.Result = new RedirectToActionResult("Index", "Employees", null);
+                }
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+    }
+
+
+    
+
+    
+}
---
+++ Data/Migrations/20200528034204_changetablename.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace trash.Data.Migrations
+{
+    public partial class changetablename : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c8f1a75a-1c0c-444a-93f4-50d73dfa2dd1", "f261deda-2278-4478-b105-b73c7ff5c2c2", "Admin", "ADMIN" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c8f1a75a-1c0c-444a-93f4-50d73dfa2dd1");
+        }
+    }
+}
